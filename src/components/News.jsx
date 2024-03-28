fb45f3550055ff2b3486848a138e39e55c129b26
matiases1995 <matiassaavedra1995@gmail.com>
2024-03-28 13:33:32 -0300

    mod: modifico algunos estilos del componente News.jsx

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,42 +1,49 @@
-import Vaquita from '../assets/images/vaquita.png'
-import Messi from '../assets/images/messi.jpg'
+import Vaquita from "../assets/images/vaquita.png";
+import Messi from "../assets/images/messi.jpg";
 
 export default function News() {
-    return (
-        <section className="news">
-          <h4>News</h4>
-          <hr />
-          {/* <!-- ULTIMO PRODUCTO AGREGADO --> */}
-          <article className="last-product">
-            <figure>
-              <img src={Vaquita} alt="product" />
-              <figcaption>
-                <h3>Nombre del vino</h3>
-              </figcaption>
-            </figure>
-            <div className="last-product-details">
-              <p className="last-product-description">
-                Un Malbec argentino de altitud, producido en los Valles
-                Calchaquíes, caracterizado por su frescura y carácter frutal.
-              </p>
-            </div>
-          </article>
-          {/* <!-- FIN ULTIMO PRODUCTO AGREGADO --> */}
-
-          {/* <!-- ULTIMO USUARIO REGISTRADO --> */}
-          <article className="last-user">
-            <figure>
-              <img src={Messi} alt="user" />
-              <figcaption>
-                <h3>Nombre del user</h3>
-              </figcaption>
-            </figure>
-            <div className="last-user-details">
-              <p className="last-user-email">Email del User</p>
-              <p className="last-user-rol">Rol del User</p>
-            </div>
-          </article>
-          {/* <!-- FIN ULTIMO USUARIO REGISTRADO --> */}
-        </section>
-    )
+  return (
+    <section className="news">
+      <h4>News</h4>
+      <hr />
+      {/* <!-- ULTIMO PRODUCTO AGREGADO --> */}
+      <article className="last-product">
+        <div>
+          <img src={Vaquita} alt="product" />
+        </div>
+        <div className="last-product-details">
+          <h2>Nombre del Producto</h2>
+          <p>
+            Pais de Origen: xxxxx
+          </p>
+          <p className="last-product-description">
+            Un Malbec argentino de altitud, producido en los Valles Calchaquíes,
+            caracterizado por su frescura y carácter frutal.
+          </p>
+          <p>
+            Tipo de Uva: xxxxx
+          </p>
+          <p>
+            Precio: $ xxxxx
+          </p>
+        </div>
+      </article>
+      {/* <!-- FIN ULTIMO PRODUCTO AGREGADO --> */}
+      <br />
+      <hr />
+      <br />
+      {/* <!-- ULTIMO USUARIO REGISTRADO --> */}
+      <article className="last-user">
+        <div>
+          <img src={Messi} alt="user" />
+        </div>
+        <div className="last-user-details">
+          <h2>Nombre del Usuario</h2>
+          <p className="last-user-email">Email del User</p>
+          <p className="last-user-rol">Rol del User</p>
+        </div>
+      </article>
+      {/* <!-- FIN ULTIMO USUARIO REGISTRADO --> */}
+    </section>
+  );
 }
