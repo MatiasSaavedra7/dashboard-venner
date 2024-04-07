6391fdb070af490ccd6ee85cf4136424f06ab419
ivana <lunaivana01@gmail.com>
2024-04-06 23:22:58 -0300

    modifico componente grapes y countries

--- src/components/Countries.jsx
+++ src/components/Countries.jsx
@@ -1,30 +1,37 @@
+import { useState, useEffect } from "react";
+
 export default function Countries() {
-    return (
-        <section className="countries">
-          <h4>Countries</h4>
-          <hr />
+  let [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    console.log("%cse montó el componente", "color: green");
+
+    // Obtener los datos iniciales (primera página)
+    fetch("http://localhost:3000/api/products")
+      .then((response) => response.json())
+      .then((data) => {
+        setCountries(data.meta.categories.countries);
+      });
+  }, []);
+
+  return (
+    <section className="countries">
+      <h4>Countries</h4>
+      <div>
+        {countries.length === 0 ? (
+          <p>Cargando...</p>
+        ) : (
           <ul>
-            <li>
-              <h5>Country 1</h5>
-              <p>Cantidad de productos: xx</p>
-            </li>
-            <li>
-              <h5>Country 2</h5>
-              <p>Cantidad de productos: xx</p>
-            </li>
-            <li>
-              <h5>Country 3</h5>
-              <p>Cantidad de productos: xx</p>
-            </li>
-            <li>
-              <h5>Country 4</h5>
-              <p>Cantidad de productos: xx</p>
-            </li>
-            <li>
-              <h5>Country 5</h5>
-              <p>Cantidad de productos: xx</p>
-            </li>
+            {countries.map((country, i) => (
+              <li key={i}>
+                <br />
+                <h3>{country.name}</h3>
+                <p>Cantidad de productos: {country.count}</p>
+              </li>
+            ))}
           </ul>
-        </section>
-    )
+        )}
+      </div>
+    </section>
+  );
 }
--- src/components/Grapes.jsx
+++ src/components/Grapes.jsx
@@ -1,30 +1,37 @@
+import { useState, useEffect } from "react";
+
 export default function Grapes() {
-    return (
-        <section className="grapes">
-          <h4>Grapes</h4>
-          <hr />
+  let [grapes, setGrapes] = useState([]);
+
+  useEffect(() => {
+    console.log("%cse montó el componente", "color: green");
+
+    // Obtener los datos iniciales (primera página)
+    fetch("http://localhost:3000/api/products")
+      .then((response) => response.json())
+      .then((data) => {
+        setGrapes(data.meta.categories.grapes);
+      });
+  }, []);
+
+  return (
+    <section className="grapes">
+      <h4>Grapes</h4>
+      <div>
+        {grapes.length === 0 ? (
+          <p>Cargando...</p>
+        ) : (
           <ul>
-            <li>
-              <h5>Grape 1</h5>
-              <p>Cantidad de productos: xx</p>
-            </li>
-            <li>
-              <h5>Grape 2</h5>
-              <p>Cantidad de productos: xx</p>
-            </li>
-            <li>
-              <h5>Grape 3</h5>
-              <p>Cantidad de productos: xx</p>
-            </li>
-            <li>
-              <h5>Grape 4</h5>
-              <p>Cantidad de productos: xx</p>
-            </li>
-            <li>
-              <h5>Grape 5</h5>
-              <p>Cantidad de productos: xx</p>
-            </li>
+            {grapes.map((grape, i) => (
+              <li key={i}>
+                <br />
+                <h3>{grape.name}</h3>
+                <p>Cantidad de productos: {grape.count}</p>
+              </li>
+            ))}
           </ul>
-        </section>
-    )
+        )}
+      </div>
+    </section>
+  );
 }
